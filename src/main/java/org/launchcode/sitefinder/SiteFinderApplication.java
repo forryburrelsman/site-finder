ec8cb2f8f3e7380a23810063cef78c5c372c92e3
Forrest <forrestburrelsman@gmail.com>
2020-08-14 14:34:29 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/sitefinder/SiteFinderApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.sitefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/sitefinder/SiteFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.sitefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
